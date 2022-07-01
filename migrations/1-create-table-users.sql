7d9356eda8c0ff76302f9278beb060527c330567
Thiago <thiago@growcode.academy>
2022-06-30 21:36:12 -0300

    cria e altera tabela de usuarios

---
+++ migrations/1-create-table-users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY,
+
+    name   VARCHAR(255) NOT NULL,
+    email  VARCHAR(255) NOT NULL,
+    pass   VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL
+);
